6f75414209fbbac320725dfbb718492effe444c5
John Kim <johnk9000@gmail.com>
2020-07-24 20:11:39 -0500

    Checking for db issues

--- server.js
+++ server.js
@@ -16,20 +16,20 @@ app.use(express.json());
 app.use(express.static("public"));
 
 // DB CONNECTION =======================================================================================================================
-if (process.env.JAWSDB_URL) {
+// if (process.env.JAWSDB_URL) {
   const connection = mysql.createConnection(process.env.JAWSDB_URL)
-} 
-const connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "Welcome_1!",
-  database: "eat_da_burger_db"
-});
+// } 
+// const connection = mysql.createConnection({
+//   host: "localhost",
+//   port: 3306,
+//   user: "root",
+//   password: "Welcome_1!",
+//   database: "eat_da_burger_db"
+// });
 
-if (process.env.JAWSDB_URL) {
-  connection = mysql.createConnection(process.env.JAWSDB_URL)
-} 
+// if (process.env.JAWSDB_URL) {
+//   connection = mysql.createConnection(process.env.JAWSDB_URL)
+// } 
 
 connection.connect(function(err) {
   if (err) {
